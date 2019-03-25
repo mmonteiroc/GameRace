a760e49f7f3d159fd9b5c42cdf3027b0097508b5
miguel <miguelmonteiroclaveri@gmail.com>
2019-03-25 21:53:27 +0100

    Circulo creado con puntuacion añadida

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,8 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.geom.Circle;
+
 public class Player {
 
     // Atributos de player
@@ -6,13 +11,31 @@ public class Player {
 
 
     // Metodos
+    //Atributos circulo
+    int x = 630;
+    int y = 670;
+    public void render(Graphics graphics, GameContainer gc){
+        Circle c = new Circle(0,0,30);
 
-    public void render(){
+        graphics.drawString("Puntuación: "+score,1100,10);
 
+        c.setX(x);
+        c.setY(y);
+        graphics.draw(c);
 
     }
 
-    public void update(){
+    public void update(GameContainer gameContainer){
+
+        Input i = gameContainer.getInput();
+        if (i.isKeyDown(Input.KEY_LEFT )&& x>=10){
+            x-=10;
+        }
+
+        if (i.isKeyDown(Input.KEY_RIGHT)&&x<=1300-70){
+            x+=10;
+        }
+
 
 
     }
--- src/RaceGame.java
+++ src/RaceGame.java
@@ -1,11 +1,17 @@
 import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Circle;
 
 public class RaceGame extends BasicGame {
     public static void main(String[] args) throws SlickException{
         // Creamos la ventana de juego
         RaceGame rg = new RaceGame("Super juego");
-        AppGameContainer aplicacion = new AppGameContainer(rg,1850,950,false);
+        AppGameContainer aplicacion = new AppGameContainer(rg,1300,750,false);
+
+        //Definimos el maximo de FPS de la ventana a 60
+        aplicacion.setTargetFrameRate(60);
         aplicacion.start();
+
+
     }
 
 
@@ -24,16 +30,16 @@ public class RaceGame extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-
+        jugador = new Player();
     }
 
     @Override
     public void update(GameContainer gameContainer, int i) throws SlickException {
-
+        jugador.update(gameContainer);
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-
+        jugador.render(graphics,gameContainer);
     }
 }
